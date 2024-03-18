81bb1ab71435d79f5556a0b6f374d6d79f2a50a4
Redwann <redwannmelsa@gmail.com>
2024-03-18 15:01:38 +0100

    adding env variables

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
