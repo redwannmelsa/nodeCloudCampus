2f47fc353b3b58b7f37f38045fbf9769b7c70814
Redwann <redwannmelsa@gmail.com>
2024-03-18 16:10:55 +0100

    trying to ignore config file for safety

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require('express')
+// const sequelize = require('sequelize')
 const app = express()
 module.exports = app;
+
+const db = require("./app/models/index.js");
+db.sequelize
+  .authenticate()
+  .then(() => console.log("Database connected ..."))
+  .catch((err) => console.log(err));
