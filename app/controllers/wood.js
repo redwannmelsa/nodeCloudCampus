bbe6fd56d57c4742303580b662624f317a3b1795
Redwann <redwannmelsa@gmail.com>
2024-03-19 16:23:17 +0100

    added list wood wontroller

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,11 @@
-exports.listWoods = (req, res) => {
-  res.send('This is a list of woods')
+const models = require('../models')
+const Wood = models.Wood
+
+exports.listWoods = async (req, res) => {
+  try {
+    const woods = await Wood.findAll()
+    res.send(woods)
+  } catch (e) {
+    res.send(e)
+  }
 }
