fce7bbdbfbe7ff5c382312ce00dca0d40501c4da
Redwann <redwannmelsa@gmail.com>
2024-03-19 14:55:01 +0100

    merging main into controllers

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express();
 
-const userCtrl = require("../controllers/users.js");
+const userCtrl = require("../controllers/user.js");
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', function (req, res) {
