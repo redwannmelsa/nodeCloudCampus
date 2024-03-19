56e0c99dbce671e4968fe231d62f3a6937b1ad59
Redwann <redwannmelsa@gmail.com>
2024-03-19 15:20:36 +0100

    addubg express json

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ db.sequelize
   .then(() => console.log("Database connected ..."))
   .catch((err) => console.log(err));
 
+app.use(express.json());
+
 const router = require("./app/routes/index.js");
 //Ajout des routes
 app.use("/api", router);
