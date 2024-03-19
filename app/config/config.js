d94248e76fb654fa7511be0a12d7239780b54c4e
Redwann <redwannmelsa@gmail.com>
2024-03-19 13:47:03 +0100

    changed fix to a better one for loading the .env in config

--- app/config/config.js
+++ app/config/config.js
@@ -1,24 +1,24 @@
-require('dotenv').config({ path: `${process.cwd()}/.env` })
+require('dotenv').config({ path: '../.env' })
 
 module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+    "database": process.env.DB_NAME_DEV,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+    "database": process.env.DB_NAME_TEST,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+    "database": process.env.DB_NAME_PROD,
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
